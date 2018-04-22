2be88dfcd997d6ab84e60f7a5b27d9d7c5e92463
Sachin <Sachin@sachin-pc>
2018-04-22 18:03:19 -0400

    -updated client file

--- src/coe817_project/client.java
+++ src/coe817_project/client.java
@@ -6,4 +6,24 @@ package coe817_project;
  */
 public class client {
     
+    private static ClientCon conn = null;
+    
+    public static void main(String[] args) {
+        
+        if(args.length == 2) {                                                  // if entered by user in terminal, for a specific server/port #
+            conn = new ClientCon(args[0], new Integer(args[1]).intValue());     // since args is text, use intValue() to convert to integer
+        }
+        else {
+            conn = new ClientCon("localhost", 21212);                           // else, just connect to server configured here
+        }
+        
+        System.out.println("Connect to: " + conn.toString());                   // display server info that connecting to     
+        String err = conn.connection();                                         // to determine if error during connection process
+        if (err.length() != 0) {                                                // if there is error message
+            System.out.println(err);                                            // display error message in terminal
+            System.exit(1);                                                     // change exit code to 1 -> catchall for general errors
+        }
+        
+    }
+    
 }
