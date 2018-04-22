344f8fa0b102b765496504631470de3b1d46b718
Sachin <Sachin@sachin-pc>
2018-04-22 18:10:22 -0400

    -updated client connection file

--- src/coe817_project/ClientCon.java
+++ src/coe817_project/ClientCon.java
@@ -1,9 +1,45 @@
 package coe817_project;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.ConnectException;
+
 /**
  *
+ * Initiates Client Connection
+ * 
  * @author Sachin
  */
 public class ClientCon {
+    
+    private String serverAddr;                                                  // initialize variables (use standard socket to connect to server)
+    private int serverPort;
+    private Socket sock = null;
+    
+    public ClientCon(String serverAddr, int serverPort) {                       // instantiate constructor to connect to specified server (instantiate variables)
+        this.serverAddr = serverAddr;
+        this.serverPort = serverPort;
+    }
+    
+    public String connection() {
+        try {
+            sock = new Socket(serverAddr, serverPort);                          // connect to specific server
+        }
+        catch (UnknownHostException e) {                                        // if IP of host can't be determined
+            return new String ("Unknown Host");
+        }
+        catch (ConnectException e) {                                            // error while trying to connect socket to remote address and port
+            return new String ("Server not running");                           // usually refused remotely (no process listening on remote address/port)
+        }
+        catch (IOException e) {                                                 // failed or interrupted I/O operations 
+            e.printStackTrace();
+            System.exit(1);                                                     // catchall for general errors
+        }
+        
+        return new String ("");                                                 // represent error length = 0 -> successful connection
+    }
     
 }
