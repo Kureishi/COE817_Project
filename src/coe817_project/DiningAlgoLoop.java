137333e7efa9642e9cf5b4a49d5f54f2cd754a68
Sachin <Sachin@sachin-pc>
2018-04-23 12:13:58 -0400

    -updated

--- src/coe817_project/DiningAlgoLoop.java
+++ src/coe817_project/DiningAlgoLoop.java
@@ -11,7 +11,7 @@ import java.util.logging.Logger;
  * @author Sachin
  */
 public class DiningAlgoLoop implements Input{
-    private final static int MAX_CHARACTER = '\uffff';                          // unsigned (max value)
+    private final static int MAX_CHARACTER = '\uffff';                          // unicode max (max 16-bit value)
     private final ClientCon connection;
 //    private Output out = null;
     
@@ -24,12 +24,12 @@ public class DiningAlgoLoop implements Input{
 //        this.out = output;
     }
     
-    public void run() {
+    public void start() {                                                       // start the loop
         Message receive;
         ArrayList<Message> roundRes;
         boolean inMessageFlag = false;
         
-        while(true) {
+        while(true) {                                                           // while nothing to break loop
             try {  
                 KeyCollection keys = getKeyCollection();                        // get the set of keys                                        
                 
@@ -40,7 +40,7 @@ public class DiningAlgoLoop implements Input{
                 receive = connection.receiveMessage();                          // configure to receive messages
                 
                 if(receive.getMsg().equals(CommProtocol.START_ROUND)) {         // if the received message indicated a "START_ROUND" message
-                    transmit(getNextChar(), keys);
+                    transmit(getNextMessageChar(), keys);
                     
                     roundRes = connection.receiveSessionResults();              // wait for results from round...
                     if(roundRes.size() == 0) {
@@ -50,9 +50,9 @@ public class DiningAlgoLoop implements Input{
                     
                     connection.send(new Message(CommProtocol.ACK));             // else, acknowledge result been received
                     
-                    char res = collate(roundRes);                               // collate(collect) results together from the round
+                    char res = collate(roundRes);                               // collate(collect) results together from the round -> compare strings based on method-defined structure
                     
-                    if(res != 0) {                                              // if a result exists
+                    if(res != 0) {                                              // if a result exists (messages present)
                         System.out.println(res);                                // display result
                         inMessageFlag = true;                                   // set Flag to true
                     } 
@@ -77,7 +77,7 @@ public class DiningAlgoLoop implements Input{
     public char collate(ArrayList<Message> message_set) {
         int sum = 0;
         
-        for(Message m: message_set) {
+        for(Message m: message_set) {                                           // for the messages in the list
             sum += Integer.parseInt(m.getMsg());                                // parse through the message and return an int to add to sum (each message) -> give idea of amount messages
         }
         
@@ -95,15 +95,57 @@ public class DiningAlgoLoop implements Input{
     }
     
     private KeyCollection getKeyCollection() throws IOException {               // private (only this class) as to not confuse with the one in KeyCollection class
-        KeyCollection keys = null;
+        KeyCollection keys = null;                                              // get collection of keys currently
         
+        try{
+            keys = connection.receiveKeyCollection();                           // read the collection from stream
+        }
+        catch(ClassNotFoundException e) {                                       // catch exception
+            return null;
+        }
         
+        connection.send(new Message(CommProtocol.ACK));                         // acknowledgement sent to server as indication that client receive collection of keys
+        
+        return null;
     }
     
+    private char getNextMessageChar() {
+        if(currMessage == null) {
+            currMessage = inBuff.next();                                        // if the current message is null (not occupied), take next String in buffer and fill it
+        }
+        
+        if(currMessage == null) {                                               // if there is nothing to be next in the buffer...
+            return (char)0;                                                     // return the character form of 0
+        }
+        else {
+            if(currMessageIndex < currMessage.length()) {                       // as long as there are still parts of message to process
+                char res_c = currMessage.charAt(currMessageIndex);              // assign the character currently processing and stored in res_c (result character)
+                currMessageIndex++;                                             // increment index to get next character of message next time
+                return res_c;                                                   // return res_c
+            }
+            else {
+                currMessage = null;                                             // once message down (no more characters) -> make the message null
+                currMessageIndex = 0;                                           // instantiate index back to 0 for next message
+                    
+                return (char)0;                                                 // return 0 as character
+            }
+        }
+    }
     
+    private void transmit(char c, KeyCollection kColl) throws IOException {
+        int cRes = c;                                                           // assign the character to transmit to cRes (to make able do integer add)
+        
+        if(c != 0) {                                                            // non-zero character (actual character)
+            cRes += MAX_CHARACTER;                                              // add max (to detect collision)
+        }
+        
+        int out_send = kColl.sum() + cRes;                                      // collect the summation of keys and the character added value
+        connection.send(new Message("" + out_send));                            // send it to the stream
+    }
     
     @Override
-    public void inputStr(String str) {
-        
+    public void inputStr(String str) {                                          // how to format the string
+        inBuff.add(str);                                                        // add string to buffer
+        System.out.println("Message: " + str + "has been added to buffer.");    // format it
     }
 }
