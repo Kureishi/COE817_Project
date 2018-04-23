5d2ba3aad9a661f76e42fa274b0637c09b4b0691
Sachin <Sachin@sachin-pc>
2018-04-23 05:22:59 -0400

    -created

---
+++ src/coe817_project/Input.java
@@ -0,0 +1,9 @@
+package coe817_project;
+
+/**
+ *
+ * @author Sachin
+ */
+public interface Input {
+    public void inputStr(String str);                                           // make interface since use in multiple places
+}
