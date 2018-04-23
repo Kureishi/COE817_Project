ea7b1293986060a1a10821fea8596ffa9291130c
Sachin <Sachin@sachin-pc>
2018-04-23 05:23:07 -0400

    -created

---
+++ src/coe817_project/Output.java
@@ -0,0 +1,9 @@
+package coe817_project;
+
+/**
+ *
+ * @author Sachin
+ */
+public interface Output {                                                       // make interface since use in multiple places
+    public void outputStr(String str);
+}
