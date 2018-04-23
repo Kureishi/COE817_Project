826ca685a36a30b9f38398be3bf587c5b4fd7448
Sachin <Sachin@sachin-pc>
2018-04-23 06:08:38 -0400

    -updated

--- src/coe817_project/DiningAlgoLoop.java
+++ src/coe817_project/DiningAlgoLoop.java
@@ -8,10 +8,16 @@ package coe817_project;
 public class DiningAlgoLoop implements Input{
     private final static int MAX_CHARACTER = '\uffff';                          // unsigned (max value)
     private final ClientCon connection;
-    private Output out = null;
+//    private Output out = null;
     
+    private StringBuffer inBuff = new StringBuffer(20);                         // specify array/buffer length to be 20 
+    private String currMessage = null;                                          // the message working on
+    private int currMessageIndex = 0;                                           // index of the current message
     
-    
+    public DiningAlgoLoop(ClientCon connection) {                               // Constructor to instantiate conversation
+        this.connection = connection;                                           // instatiate variables
+//        this.out = output;
+    }
     
     
     
